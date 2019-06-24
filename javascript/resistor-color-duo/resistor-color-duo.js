09c48786cb9053cf3a8f08ea4a0112a6692e700a
Reuben <reuben@koppenheffer.me>
2019-06-24 09:27:55 -0500

    Finish resister-color-duo

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const value = colors => {
+  let code1 = COLORS.indexOf(colors[0]).toString();
+  let code2 = COLORS.indexOf(colors[1]).toString();
+  return parseInt(code1 + code2);
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
