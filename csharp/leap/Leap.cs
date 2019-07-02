4491b5d43f122800b7de8693475977226a0df072
Reuben <reuben@koppenheffer.me>
2019-07-01 19:15:58 -0500

    Complete LeapTest exercise

---
+++ csharp/leap/Leap.cs
@@ -0,0 +1,20 @@
+using System;
+
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+
+        // on every year that is evenly divisible by 4
+        // except every year that is evenly divisible by 100
+        // unless the year is also evenly divisible by 400    
+
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+        {
+            return true;
+        }
+
+        return false;
+
+    }
+}
