eb65cf115035965e7545acd418415bf0af7c9a01
Reuben <reuben@koppenheffer.me>
2019-07-15 07:58:25 -0500

    Finish reverse string

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        string reversed = "";
+        int length = input.Length - 1;
+        while (length >= 0)
+        {
+            reversed += input[length];
+            length--;
+        }
+        return reversed;
+    }
+}
