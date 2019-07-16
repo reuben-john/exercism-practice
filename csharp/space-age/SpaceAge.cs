3011a5e8f6075fb69409784c76945f360e7e636d
Reuben <reuben@koppenheffer.me>
2019-07-16 06:31:41 -0500

    Replaced dictionary with consts and changed one line function format

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -5,66 +5,37 @@ public class SpaceAge
 {
 
     private double _seconds;
-    private double _earthYearInSeconds;
-    private Dictionary<string, double> _spaceFormulas;
-    public SpaceAge(int seconds)
-    {
-        _seconds = seconds;
-        _earthYearInSeconds = 31557600;
-        _spaceFormulas = new Dictionary<string, double> {
-            {"Earth", 1},
-            {"Mercury", 0.2408467},
-            {"Venus", 0.61519726},
-            {"Mars", 1.8808158},
-            {"Jupiter", 11.862615},
-            {"Saturn", 29.447498},
-            {"Uranus", 84.016846},
-            {"Neptune", 164.79132}
-        };
-    }
+    private double earthYearInSeconds = 31_557_600;
 
-    public double calculateSpaceAge(double seconds, double year, double formula)
+    public class SpaceFormulas
     {
-        return Math.Round(seconds / (year * formula), 2);
+        public const double Earth = 1;
+        public const double Mercury = 0.2408467;
+        public const double Venus = 0.61519726;
+        public const double Mars = 1.8808158;
+        public const double Jupiter = 11.862615;
+        public const double Saturn = 29.447498;
+        public const double Uranus = 84.016846;
+        public const double Neptune = 164.79132;
     }
+    public SpaceAge(int seconds) => _seconds = seconds;
 
-    public double OnEarth()
-    {
-        return calculateSpaceAge(_seconds, _earthYearInSeconds, _spaceFormulas["Earth"]);
-    }
 
-    public double OnMercury()
-    {
-        return calculateSpaceAge(_seconds, _earthYearInSeconds, _spaceFormulas["Mercury"]);
-    }
+    public double calculateSpaceAge(double seconds, double year, double formula) => Math.Round(seconds / (year * formula), 2);
 
-    public double OnVenus()
-    {
-        return calculateSpaceAge(_seconds, _earthYearInSeconds, _spaceFormulas["Venus"]);
-    }
+    public double OnEarth() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Earth);
 
-    public double OnMars()
-    {
-        return calculateSpaceAge(_seconds, _earthYearInSeconds, _spaceFormulas["Mars"]);
-    }
+    public double OnMercury() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Mercury);
 
-    public double OnJupiter()
-    {
-        return calculateSpaceAge(_seconds, _earthYearInSeconds, _spaceFormulas["Jupiter"]);
-    }
+    public double OnVenus() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Venus);
 
-    public double OnSaturn()
-    {
-        return calculateSpaceAge(_seconds, _earthYearInSeconds, _spaceFormulas["Saturn"]);
-    }
+    public double OnMars() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Mars);
 
-    public double OnUranus()
-    {
-        return calculateSpaceAge(_seconds, _earthYearInSeconds, _spaceFormulas["Uranus"]);
-    }
+    public double OnJupiter() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Jupiter);
 
-    public double OnNeptune()
-    {
-        return calculateSpaceAge(_seconds, _earthYearInSeconds, _spaceFormulas["Neptune"]);
-    }
+    public double OnSaturn() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Saturn);
+
+    public double OnUranus() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Uranus);
+
+    public double OnNeptune() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Neptune);
 }
