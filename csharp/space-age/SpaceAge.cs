f4ee5d836596dc48f294a179f8c0fc0f86f562aa
Reuben <reuben@koppenheffer.me>
2019-07-17 05:44:56 -0500

    Change SpaceFormulas to private and move to bottom

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -7,17 +7,6 @@ public class SpaceAge
     private double _seconds;
     private double earthYearInSeconds = 31_557_600;
 
-    public class SpaceFormulas
-    {
-        public const double Earth = 1;
-        public const double Mercury = 0.2408467;
-        public const double Venus = 0.61519726;
-        public const double Mars = 1.8808158;
-        public const double Jupiter = 11.862615;
-        public const double Saturn = 29.447498;
-        public const double Uranus = 84.016846;
-        public const double Neptune = 164.79132;
-    }
     public SpaceAge(int seconds) => _seconds = seconds;
 
 
@@ -38,4 +27,16 @@ public class SpaceAge
     public double OnUranus() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Uranus);
 
     public double OnNeptune() => calculateSpaceAge(_seconds, earthYearInSeconds, SpaceFormulas.Neptune);
+
+    private class SpaceFormulas
+    {
+        public const double Earth = 1;
+        public const double Mercury = 0.2408467;
+        public const double Venus = 0.61519726;
+        public const double Mars = 1.8808158;
+        public const double Jupiter = 11.862615;
+        public const double Saturn = 29.447498;
+        public const double Uranus = 84.016846;
+        public const double Neptune = 164.79132;
+    }
 }
