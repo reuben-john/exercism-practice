80fc224ab28fd3cc9b78830ea9caa56384c3f8e0
Reuben <reuben@koppenheffer.me>
2019-07-21 05:23:31 -0500

    Change function syntax and copy list

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -6,28 +6,18 @@ public class HighScores
 {
 
     private List<int> _highScores;
-    public HighScores(List<int> list)
-    {
-        _highScores = list;
-    }
+    public HighScores(List<int> list) => _highScores = new List<int>(list);
 
-    public List<int> Scores()
-    {
-        return _highScores;
-    }
 
-    public int Latest()
-    {
-        return _highScores.Last();
-    }
+    public List<int> Scores() => _highScores;
 
-    public int PersonalBest()
-    {
-        return _highScores.Max();
-    }
 
-    public List<int> PersonalTopThree()
-    {
-        return _highScores.OrderByDescending(score => score).Take(3).ToList();
-    }
+    public int Latest() => _highScores.Last();
+
+
+    public int PersonalBest() => _highScores.Max();
+
+
+    public List<int> PersonalTopThree() => _highScores.OrderByDescending(score => score).Take(3).ToList();
+
 }
